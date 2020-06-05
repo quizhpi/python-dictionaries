31af319e31d0c87f4f7d4bc7363304c3dad0beb8
Manny <mquizhpidev@gmail.com>
2020-06-04 20:28:23 -0400

    Access disctionary based on user input

--- app.py
+++ app.py
@@ -2,7 +2,18 @@
 # Dictionary with python
 #
 
-# Create and access a dictionary
+# Access all key value pairs of a dictionary
+
+sports = {
+    1: "Basketball",
+    2: "Skateboard",
+    3: "Ping pong"
+}
+
+for sport in sports:
+    print(str(sport) + ": " + sports[sport])
+
+# Create and access a dictionary based on user input
 
 car_shop = {
     "gas": "Ford Focus",
@@ -10,8 +21,18 @@ car_shop = {
     "electric": "Tesla Model X"
 }
 
-car_choice = input("What type of car do you prefer: gas, hybrid or electric: ")
+print("")
 
-print("Based on your selection, the following car is recommended")
-print("---------------------------------------------------------")
-print(car_shop[car_choice])
+no_answer = True
+
+while no_answer:
+    car_choice = input("What type of car do you prefer: gas, hybrid or electric: ")
+
+    if car_choice.lower() == "gas" or car_choice.lower() == "hybrid" or car_choice.lower() == "electric":
+        print("---------------------------------------------------------")
+        print("Based on your selection, the following car is recommended")
+        print("---------------------------------------------------------")
+        print(car_shop[car_choice.lower()])
+        no_answer = False
+    else:
+        print("Try typing in one of the three options provided.")
